dc215a993c583175d801a3911e377015b861ec75
SoporteDB <127426310+SoporteDB@users.noreply.github.com>
2023-03-27 15:20:25 -0500

    Add files via upload

---
+++ api-mongo/src/main/java/com/dba/demo/service/PromocionService.java
@@ -0,0 +1,42 @@
+package com.dba.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.json.JsonObject;
+import org.springframework.stereotype.Service;
+
+import com.dba.demo.model.Promocion;
+import com.dba.demo.repository.PromocionRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PromocionService {
+	
+	private final PromocionRepository promocionRespository;
+	
+	public void add(Promocion promocion) {
+		System.out.println("esta es la campaña "+ promocion);
+		//JsonObject response = new JsonObject()
+		promocionRespository.save(promocion);
+	}
+	
+	
+	public List<Promocion>findAll(){
+		return promocionRespository.findAll();
+	}
+	
+	public Optional<Promocion> findById(String id) {
+		return promocionRespository.findById(id);
+	}
+	
+	public void deleteById(String id) {
+		promocionRespository.deleteById(id);
+	}
+
+	
+	
+	
+}
